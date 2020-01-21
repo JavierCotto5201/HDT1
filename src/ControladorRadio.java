affb0196441b80dcf3daddeff304ae3f92ca67ad
Jose Hurtarte <JJHH121106@gmail.com>
2020-01-21 16:54:59 -0600

    Creacion de ControladorRadio

---
+++ src/ControladorRadio.java
@@ -0,0 +1,60 @@
+/**
+ * @author josej
+ *
+ */
+
+
+public class ControladorRadio implements iRadio {
+	private boolean encendido;
+	private int frecuencia;
+
+	/**
+	 * 
+	 */
+	public ControladorRadio() {
+		
+	}
+
+	@Override
+	public String estacionActual() {
+	
+		return null;
+	}
+
+	@Override
+	public boolean estado() {
+	
+		return false;
+	}
+
+	@Override
+	public void onOff() {
+		
+
+	}
+
+	@Override
+	public void cambiarFrecuencia() {
+		
+
+	}
+
+	@Override
+	public void avanzar() {
+		
+
+	}
+
+	@Override
+	public void guardar(int boton) {
+		
+
+	}
+
+	@Override
+	public void seleccionarEmisora(int boton) {
+		
+
+	}
+
+}
